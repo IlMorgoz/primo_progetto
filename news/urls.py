92c4f3eb61ca09494d65b835d61fcefd0d886304
IlMorgoz <157831047+IlMorgoz@users.noreply.github.com>
2026-01-19 12:29:16 +0100

    Modificata app news con duplice url

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, articoloDetailView
+from .views import home, articoloDetailView, listaArticoli
 app_name = 'news'
 urlpatterns = [
     path("", home, name="home"),
-    path("articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
+    path("/articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
+    path("/lista_articoli", listaArticoli, name="lista_articoli"),
+
+    path("/lista_articoli/<int:pk>", listaArticoli, name="lista_articoli"),
 ]
