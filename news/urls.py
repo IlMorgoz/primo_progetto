1e065049fa8afdadae5143418317eaf01513fc5c
IlMorgoz <157831047+IlMorgoz@users.noreply.github.com>
2026-01-22 15:39:25 +0100

    commit generico

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home, articoloDetailView, listaArticoli
+from .views import home, articoloDetailView, listaArticoli, queryBase
+
 app_name = 'news'
 urlpatterns = [
     path("", home, name="home"),
@@ -7,4 +8,5 @@ urlpatterns = [
     path("/lista_articoli", listaArticoli, name="lista_articoli"),
 
     path("/lista_articoli/<int:pk>", listaArticoli, name="lista_articoli"),
+    path("/query_base",queryBase, name="query_base"),
 ]
